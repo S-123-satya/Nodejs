cc0f9a76a182c0cb39531d1efdf2c88e0c170c40
S-123-satya <satyaprakash5056742@gmail.com>
2023-07-01 05:14:16 +0530

    data save and display to website after redirecting

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const http=require('http');
+const fs=require('fs');
 
 const server=http.createServer((req,res)=>{
     const url=req.url;
-    if(url=='/home')
-        return res.end('welcome to home page');
-    else if(url=='/about')
-        return res.end('welcome to about us');
-    else if(url=='/node')
-        return res.end('welcome to node server');
-    return res.end(`Error 404, page not found`);
+    if(url==='/'){
+        fs.readFile('satya.txt','utf-8',(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+            if(data===undefined)
+                data='';
+            res.write(`<html>`);
+            res.write(
+                `<body>${data}<form action='/home' method='post'><input type='text' name='message'><button type='submit'>send</button></form></body>`
+            );
+            res.write(`</html>`);
+            res.end();
+        });
+        
+    }
+    if(url==='/home'){
+        const body=[];
+        req.on('data',(chunk)=>{
+            body.push(chunk);
+        });
+        return req.on('end',()=>{
+            const parsedata=Buffer.concat(body).toString();
+            console.log(parsedata);
+            const result = parsedata.split('=')[1];
+            fs.writeFileSync('satya.txt',result);
+            res.statusCode=302;
+            res.setHeader('Location','/');
+            return res.end();
+        });
+    }
+        
 });
-server.listen(4000,'127.0.0.1',()=>{
-    console.log('listening on port 4000');
-});
+server.listen(4000);
